db76e06aaf9cdfbdc6b03dc5a3b29f0886393dc5
Emily <emilyfan@MacBook-Pro-4.local>
2018-03-04 19:56:24 -0800

    added a time display element on the top of the page (really ugly) the timer works ish, made run at document_end instead of document start so the elements are loaded

--- content.js
+++ content.js
@@ -1,15 +1,72 @@
-//alert("content.js entered");
-sentfromExtension = false;
+var timeDisplay = document.createElement("p");
+timeDisplay.appendChild(document.createTextNode("TEXT"));
+timeDisplay.style.float = "left";
 
-/*chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    alert(sender);
-    alert(request);
-    sendResponse({received: "clicked"});
-});*/
 
-chrome.runtime.sendMessage({onFacebook: true}, function(response){
-   console.log(response); 
-});
+var startTime;
+var endTime;
 
-/*chrome.tabs.executeScript(null, {"file": "removeChat.js"});
-chrome.tabs.executeScript(null, {"file": "removeTitleNotifications.js"});*/
+//var displayTime;
+
+var accumulatedTime;
+var accumulatedTimeKey='accTimeMS'
+
+function startTimer(){
+    startTime = performance.now();
+    
+    accumulatedTime = chrome.storage.sync.get(accumulatedTimeKey, function(items){
+        return items[accumulatedTimeKey];
+        /*console.log(chrome.runtime.lastError);
+        console.log(items[accumulatedTimeKey]);
+        
+        return (chrome.runtime.lastError ? 0 : items[accumulatedTimeKey]);*/
+    });
+    
+    if(accumulatedTime == undefined) accumulatedTime = 0;
+    console.log(accumulatedTime);
+}
+
+function endTimer(){
+    endTime = performance.now();
+    
+    accumulatedTime+=(endTime-startTime);
+    
+    var items = {};
+    item[accumulatedTimeKey] = accumulatedTime;
+    
+    chrome.storage.sync.set(items);
+    //add some mech to store it to chrome
+}
+
+function updateTime(){   
+    var now = performance.now();
+    console.log(now);
+    
+    var secondsPassed = (now - startTime + accumulatedTime)/1000;
+    console.log(startTime)
+    console.log(accumulatedTime);
+    console.log(secondsPassed);
+    var minutes = Math.floor(secondsPassed / 60);
+    var seconds = Math.floor(secondsPassed % 60);
+    
+    var displayString;
+    
+    if(seconds < 10)
+        displayString= minutes + ":0" + seconds;
+    else
+        displayString= minutes + ":" + seconds;
+    //alert(displayString);
+    timeDisplay.innerHTML = displayString;
+}
+
+fbTopBar = document.getElementById("pagelet_bluebar");
+
+//console.log(fbTopBar);
+
+//fbTopBar.insertBefore(timeDisplay, fbTopBar.firstChild);
+fbTopBar.appendChild(timeDisplay);
+
+
+
+startTimer();
+setInterval(updateTime, 1000);
