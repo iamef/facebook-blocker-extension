0cd22bbffd2b0d8fe07577750171aa8507234b48
Emily <emilyfan@MacBook-Pro-4.local>
2018-03-04 21:52:45 -0800

    deleted start and end time and made everything into accumulated time attempted message receiving (failed) got the storage part to work

--- content.js
+++ content.js
@@ -1,51 +1,64 @@
 var timeDisplay = document.createElement("p");
 timeDisplay.appendChild(document.createTextNode("TEXT"));
-timeDisplay.style.float = "left";
+//timeDisplay.style.float = "left";
 
-
-var startTime;
-var endTime;
 
 //var displayTime;
 
 var accumulatedTime;
 var accumulatedTimeKey='accTimeMS'
 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+      //console.log(request);
+      alert("request Received");
+      sendResponse({receved: true});
+  });
+
+
 function startTimer(){
-    startTime = performance.now();
-    
-    accumulatedTime = chrome.storage.sync.get(accumulatedTimeKey, function(items){
-        return items[accumulatedTimeKey];
+    chrome.storage.sync.get(accumulatedTimeKey, function(items){
+        console.log(items[accumulatedTimeKey]);
+        accumulatedTime = items[accumulatedTimeKey];
         /*console.log(chrome.runtime.lastError);
         console.log(items[accumulatedTimeKey]);
         
         return (chrome.runtime.lastError ? 0 : items[accumulatedTimeKey]);*/
     });
+    console.log(accumulatedTime);
     
     if(accumulatedTime == undefined) accumulatedTime = 0;
     console.log(accumulatedTime);
 }
 
-function endTimer(){
-    endTime = performance.now();
-    
-    accumulatedTime+=(endTime-startTime);
-    
+function storeAccumulatedTime(){
     var items = {};
-    item[accumulatedTimeKey] = accumulatedTime;
+    items[accumulatedTimeKey] = accumulatedTime;
     
-    chrome.storage.sync.set(items);
-    //add some mech to store it to chrome
+    console.log(items);
+    
+    chrome.storage.sync.set(items, function(){
+        console.log("saved " + accumulatedTime);
+        
+        
+    });
 }
 
-function updateTime(){   
-    var now = performance.now();
-    console.log(now);
+
+
+function updateTime(){
+    accumulatedTime+=1;
+    storeAccumulatedTime();
     
-    var secondsPassed = (now - startTime + accumulatedTime)/1000;
-    console.log(startTime)
-    console.log(accumulatedTime);
-    console.log(secondsPassed);
+    chrome.storage.sync.get(accumulatedTimeKey, function(items){
+        console.log(items[accumulatedTimeKey]);
+    });
+    
+    
+    var secondsPassed = accumulatedTime;
+    //console.log(startTime)
+    //console.log(accumulatedTime);
+    //console.log(secondsPassed);
     var minutes = Math.floor(secondsPassed / 60);
     var seconds = Math.floor(secondsPassed % 60);
     
@@ -63,8 +76,8 @@ fbTopBar = document.getElementById("pagelet_bluebar");
 
 //console.log(fbTopBar);
 
-//fbTopBar.insertBefore(timeDisplay, fbTopBar.firstChild);
-fbTopBar.appendChild(timeDisplay);
+fbTopBar.insertBefore(timeDisplay, fbTopBar.firstChild);
+//fbTopBar.appendChild(timeDisplay);
 
 
 
