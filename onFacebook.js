8877301e0ffc3364102ca014090dcfcde878eaf3
work <emilyfan8@gmail.com>
2018-04-17 22:27:46 -0700

    manifest.json/onFacebookListener.js/onFacebook.js: added new onFacebookListener.js background page to unblock when content script onFacebook.js sends a message to it using content css files (below)
    chatContent.css: mostly just added comments for clarity/future reference to add more functions
    
    undoChatContentReg.css: Added
    
    undoNotificationsContent.css: Added

---
+++ onFacebook.js
@@ -0,0 +1,3 @@
+chrome.runtime.sendMessage({onFacebook: true}, function(response){
+    //alert("received: " + response.received);
+});
---
+++ onFacebookListener.js
@@ -0,0 +1,26 @@
+var enabledKey='enabled';
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+    if(message.onFacebook){
+        //alert("message: " + message.onFacebook);
+        chrome.storage.sync.get(enabledKey, function(items){
+           runEnabled(items[enabledKey]); //check if it is enabled and does what it should 
+        });
+
+        sendResponse({received: true});
+    }
+});
+
+
+//Enabled settings
+function runEnabled(enabled){
+    if(!enabled){
+        //need to make sure that notifications are on and chat is on
+        chrome.tabs.insertCSS(null, {file: "undoChatContentReg.css"});
+        chrome.tabs.insertCSS(null, {file: "undoNotificationsContent.css"});
+    }
+}
+
+
+//Blocking settings
+//blocked items
+//chat sidebar (search isn't blocked): 
